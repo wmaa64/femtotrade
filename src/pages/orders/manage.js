1fbe77244fe5d998e56e06453ba56460746ff1b4
wael mohamed aly <wmaa64@yahoo.com>
2026-04-05 15:57:54 +0200

    5-4-26-3

--- src/pages/orders/manage.js
+++ src/pages/orders/manage.js
@@ -144,7 +144,7 @@ const ManageOrders = () => {
                             <strong>{(item.selectedCategories.length > 0) ? item.displayName : ""}  </strong>
                           </p>
                           <p style={{ margin: 0 }}>
-                            Qty: {item.quantity} × {item.price} EGP = {item.quantity * item.price +(50)} EGP
+                            Qty: {item.quantity} × {item.price} EGP = {item.quantity * item.price} EGP
                           </p>
                         </div>
                       </div>
