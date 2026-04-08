624488c35bdc4d03289ccd6ce71e0f84773e5e39
wael mohamed aly <wmaa64@yahoo.com>
2026-04-08 15:45:23 +0200

    8-4-26-9

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,23 +8,39 @@ const About = () => {
       description="Great tasting home made macarons"
     />
     <div className="about-us">
-        <p>About Macaron Magic</p>
+        <p>Specialized in German Pets Suppliers Specialy in  Organic Litter for home animals</p>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate
-            justo ac tellus egestas dictum. Aenean vestibulum diam eu risus cursus
-            mollis. Nulla dapibus ante in felis vulputate mattis. Mauris sed lacus
-            eget sapien rutrum interdum. Proin a semper magna. Aliquam eget purus
-            fringilla, rutrum augue at, porta sem.
-        </p>
+            Our products has these Features:
+            Cat's Best - Organic Litter - Smart Pellets - Prevent Tracking
+            Organic Litter - Original - active against urine and odours
+            Sensitive - Naturally Anti-bacterial
+            The Power of Nature - Confort - Non Clumping & High ABsorption
+            Universal - Non Clumping & High ABsorption
+            
+            Dog's Best - 100% natural organic fibres - 
+            the ideal supplement to taking dog out to "do its business - 
+            Dog freindly confort round the clock - super absorbent - 
+            stops odeur - 100% biodegradable compostable.        </p>
         <p>
-            Aenean aliquam lectus tellus, sed venenatis nulla pretium ac. 
-            Vivamus cursus purus quam, eu placerat est rutrum quis. 
-            Nunc sit amet urna sed libero auctor imperdiet nec consequat sem. 
-            Quisque mauris est, fermentum in tristique quis, tincidunt a odio. 
-            Fusce porttitor eu est interdum consectetur. 
-            Ut aliquam semper dui, vulputate hendrerit sapien interdum sit amet. 
-            Nam vitae nulla et lorem ornare auctor. Nulla facilisi. Nam sed sollicitudin leo.
-        </p>
+ تتميز منتجاتنا بالميزات التالية:
+
+أفضل رمل للقطط - رمل عضوي - حبيبات ذكية - يمنع التتبع
+
+رمل عضوي - أصلي - فعال ضد البول والروائح
+
+لطيف على البشرة الحساسة - مضاد للبكتيريا بشكل طبيعي
+
+قوة الطبيعة - راحة - غير متكتل وعالي الامتصاص
+
+مناسب لجميع أنواع الكلاب - غير متكتل وعالي الامتصاص
+
+أفضل رمل للكلاب - ألياف عضوية طبيعية 100% -
+
+المكمل المثالي لإخراج الكلب لقضاء حاجته -
+
+راحة مثالية للكلاب على مدار الساعة - امتصاص فائق -
+
+يمنع الروائح - قابل للتحلل الحيوي والتسميد بنسبة 100%.       </p>
     </div>
     </>
   );
