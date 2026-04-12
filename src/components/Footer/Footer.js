692c19f82ed72bbef8898b439354915b2504663e
wael mohamed aly <wmaa64@yahoo.com>
2026-04-12 19:20:08 +0200

    12-4-26-11

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -45,7 +45,7 @@ const Footer = () => {
                     <a href="https://www.facebook.com/share/1Nvayb9uW8" target="_blank" rel="noopener noreferrer">
                         <AiFillFacebook    size={30} color="#1877F2" /> {/* Facebook */}
                     </a>
-                    <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer">
+                    <a href="https://wa.me/+201005126629" target="_blank" rel="noopener noreferrer">
                         <AiOutlineWhatsApp size={30} color="#25D366" /> {/* WhatsApp */}
                     </a>
                 </div>
