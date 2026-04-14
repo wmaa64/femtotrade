4988b4958afa185605a73586691b3dffdd64e6d0
wael mohamed aly <wmaa64@yahoo.com>
2026-04-14 02:30:36 +0200

    14-4-26-2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteUrl: 'https://femtotrade.shop',
   generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
   exclude: [
     '/contact',
     'canceled',
@@ -11,8 +13,27 @@ module.exports = {
     '/shop',
     '/success',
     '/terms',
-    '/users/*',
-  ],
-  changefreq: 'daily',
-  priority: 0.7,
+    '/users/*'
+  ]
+  
+  ,robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+            '/contact',
+            'canceled',
+            '/delivery',
+            '/orders/manage',
+            '/privacy',
+            '/products/manage',
+            '/shop',
+            '/success',
+            '/terms',
+            '/users/*'
+        ],
+      },
+    ],
+  },
 }
