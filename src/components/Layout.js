08d6735087b48aef52717cc9a5edbb61bf2a7f9b
wael mohamed aly <wmaa64@yahoo.com>
2026-04-26 11:45:30 +0300

    26-4-26-1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Script from "next/script"
 import Header from "../components/Header"
 import NavBar  from "./NavBar";
 
@@ -9,7 +10,6 @@ const Layout = ({ children }) => {
 return (
 <>
     <Head>
-
         {/* الأساسيات */}
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -50,23 +50,21 @@ return (
         {/* ملفات */}
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/favicon/favicon32.png" color="#5bbad5" />
-
-        {/*<meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="google-site-verification" content="WUlB-DC4Ep631yNAivYsTKsJL9q2tN8O0hlLFJcES8c" />
-        <link rel="canonical" href="https://femtotrade.shop/" />
-        <title>شركة متخصصة فى  فرش القطط و الكلاب والحيوانات الاليفة فى مصر - فرش قطط اورجنال </title>
-        <meta name="description" content="تسوق منتج المانى عالى الجودة - فرش القطط و الكلاب والحيوانات الاليفة فى مصر - اورجنال - 
-    اكتيف ضد البول والروائح - 100% طبيعي - 100% قابل للتحلل - خالي من الغبار - طويل الأمد - مكافحة الروائح - سهل التنظيف!" />        
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon"  sizes="32x32" href="/favicon/favicon32.png" />
-        <link rel="icon"  sizes="16x16" href="/favicon/favicon16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/favicon32.png" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />*/}
-
     </Head>
+
+    {/* Google Analytics */}
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=532716911"
+        strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '532716911');
+        `}
+    </Script>
 
     <header>
         <Header />
